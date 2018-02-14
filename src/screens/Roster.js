1c37aa2f2a92053a3c58b48619832de6225ea1d8
galenriley <galenriley@gmail.com>
2018-02-13 21:47:01 -0500

    stub Roster

---
+++ src/screens/Roster.js
@@ -0,0 +1,2 @@
+// This is basically going to be the Speakers -> Details screens from the original Nodevember app
+// Our implementation is Roster -> Player (player screen will have the player's song(s) as well)
