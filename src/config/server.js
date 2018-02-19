23fbd058c417f8cf26d615f850c6783322ef0a5d
galenriley <galenriley@gmail.com>
2018-02-18 20:31:15 -0500

    using test API endpoint

--- src/config/server.js
+++ src/config/server.js
@@ -1,4 +1,7 @@
-export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com';
+// Production https://chattahooligan-hymnal.herokuapp.com
+// Dev https://hymn-test.herokuapp.com
+
+export const HYMNAL_ADDRESS = 'https://hymn-test.herokuapp.com';
 // process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:3000'
 //   : 'https://chattahooligan-hymnal.herokuapp.com';
