2da7dec68e9df4751bf2bd7edb2e965bb1593906
Zachary Parrish <zacharyparrish@protonmail.com>
2018-02-25 21:32:08 -0500

    Changing Master server value

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,8 @@
 // Production https://chattahooligan-hymnal.herokuapp.com
 // Dev https://hymn-test.herokuapp.com
 
-export const HYMNAL_ADDRESS = 'https://hymn-test.herokuapp.com';
+export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
+  //= 'https://hymn-test.herokuapp.com';
 // process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:3000'
 //   : 'https://chattahooligan-hymnal.herokuapp.com';
