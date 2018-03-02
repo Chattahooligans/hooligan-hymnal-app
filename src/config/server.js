8c759c36e78cf57e4e07940b3d143e6ab4097647
galenriley <galenriley@gmail.com>
2018-03-02 15:41:53 -0500

    "production" configuration

--- src/config/server.js
+++ src/config/server.js
@@ -1,7 +1,9 @@
-// Production https://chattahooligan-hymnal.herokuapp.com
-// Dev https://hymn-test.herokuapp.com
-
+// Production
 export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
+
+// Dev
+//export const HYMNAL_ADDRESS = 'https://hymn-test.herokuapp.com'
+
   //= 'https://hymn-test.herokuapp.com';
 // process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:3000'
