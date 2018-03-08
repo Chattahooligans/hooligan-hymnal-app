8db3849cf2ffa0bce36751d4b2eae5b62672ca6e
galenriley <galenriley@gmail.com>
2018-03-08 15:40:25 -0500

    stub out skinning

---
+++ src/skin/Colors.js
@@ -0,0 +1,5 @@
+// Chattahooligans palette
+export const Colors = {
+    Navy: '#032E55',
+    Sky: '#A5D8F6'
+};
---
+++ src/skin/Styles.js
@@ -0,0 +1,6 @@
+import Colors from './Colors';
+
+export const GlobalNavigationBar = Colors.Navy;
+
+export const SongbookNavigationBar = GlobalNavigationBar;
+export const RosterNavigationBar = GlobalNavigationBar;
