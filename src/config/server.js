02ce117205952b72f3bd9ed236d962db5e0a056a
Galen Riley <galenriley@gmail.com>
2019-10-28 19:02:17 -0400

    change server address back (should go in config)

--- src/config/server.js
+++ src/config/server.js
@@ -3,7 +3,7 @@
 //export const HYMNAL_ADDRESS = 'https://radiant-citadel-22556.herokuapp.com'
 
 // Dev
-export const HYMNAL_ADDRESS = 'https://guardbook-beta.herokuapp.com'
+export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
 
   //= 'https://hymn-test.herokuapp.com';
 // process.env.NODE_ENV === 'development'
