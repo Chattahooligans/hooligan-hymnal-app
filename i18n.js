7c1cbf4b125d91d2db8b40502640580273519304
Sam Shrum <Sam.L.Shrum@gmail.com>
2019-10-28 07:04:35 -0400

    Get user's current locale

--- i18n.js
+++ i18n.js
@@ -1,17 +1,21 @@
 //configure internationalization
 import i18n from 'i18n-js';
+import * as Localization from 'expo-localization';
 //import files
 import en from './locales/en.json';
-//default to English; if a locale is missing a string,
-//read it from English
+//default to English
 i18n.defaultLocale = 'en';
-i18n.locale = 'en';
+//get user's current locale
+i18n.locale = Localization.locale;
+//if not found in the current locale, fallback to English
 i18n.fallbacks = true;
 i18n.translations = { en };
+//future: check Localization.isRTL
+//future: when app returns from backgroud on Android,
+//check `await Localization.getLocalizationAsync();` to update locale
 
 function getLocalizedBio(bio) {
     //if bio is only a string, return that string
-    console.log("localizing");
     if(typeof(bio) === "string") return bio;
     //otherwise, see if the current locale is in there.
     var currentLocale = i18n.currentLocale();
