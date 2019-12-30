0bfb9ef4e1240edfa2704fb603ecc5a3cce87b8d
Galen Riley <galenriley@gmail.com>
2019-12-30 18:35:52 -0500

    dev branch config

--- src/config/server.js
+++ src/config/server.js
@@ -1,11 +1,11 @@
 // Production
 //export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
-//export const HYMNAL_ADDRESS = 'https://radiant-citadel-22556.herokuapp.com'
 
-// Dev
-export const HYMNAL_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
+// news feed dev branch
+//export const HYMNAL_ADDRESS = 'https://hymnal-serve-news-feed-vf0sbpf.herokuapp.com'
 
-  //= 'https://hymn-test.herokuapp.com';
+export const HYMNAL_ADDRESS = 'https://hymnal-serve-news-feed-vf0sbpf.herokuapp.com'
+
 // process.env.NODE_ENV === 'development'
 //   ? 'http://localhost:3000'
 //   : 'https://chattahooligan-hymnal.herokuapp.com';
