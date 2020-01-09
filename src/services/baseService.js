4b1aa125fa90375952283ab09553358f818dcc35
Galen Riley <galenriley@gmail.com>
2020-01-08 20:09:46 -0500

    add logging interceptor to axiois

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -1,10 +1,20 @@
 import axios from 'axios';
 import { HYMNAL_ADDRESS } from '../config/server';
 
-export default axios.create({
+let API = axios.create({
   baseURL: HYMNAL_ADDRESS,
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
   }
 });
+API.interceptors.request.use(request => {
+  console.log('Starting Request\n', request)
+  return request
+})
+API.interceptors.response.use(response => {
+  console.log('Response\n', response)
+  return response
+})
+
+export default API;
