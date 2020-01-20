4ee3b0b008f1d941ffc69f1b1b2f2ff2685d2ee5
Galen Riley <galenriley@gmail.com>
2020-01-20 14:00:38 -0500

    change singlepost screen title

--- src/screens/SinglePost.js
+++ src/screens/SinglePost.js
@@ -11,7 +11,7 @@ import { DefaultColors, Palette, Skin } from '../config/Settings';
 
 export default class SinglePost extends React.Component {
     static navigationOptions = ({ navigation }) => ({
-        title: "View Post",
+        title: "[" + appParams.expo.name + "] View Post",
         ...NavigationOptions,
         headerLeft: (
             <HeaderBackButton onPress={() => navigation.goBack()} tintColor="#fff" />
