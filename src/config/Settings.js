b20e072afa2767917e215a3595272c01c560c898
Galen Riley <galenriley@gmail.com>
2020-01-23 18:05:23 -0500

    tacoman operational experience

--- src/config/Settings.js
+++ src/config/Settings.js
@@ -7,6 +7,8 @@
 
 // TODO: Build home screen dynamically based on this websites object
 
+// HEY! Don't include a trailing slash / on this url. 
+// If you do, you're gonna have a bad time because your serer calls will not work
 export const HOOLIGAN_HYMNAL_SERVER_ADDRESS = 'https://chattahooligan-hymnal.herokuapp.com'
 // news feed dev branch
 //export const HOOLIGAN_HYMNAL_SERVER_ADDRESS = 'https://hymnal-serve-news-feed-vf0sbpf.herokuapp.com'
