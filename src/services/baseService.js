c9af091cc8d7c13830b14a4cb1df45fb23e6e020
Galen Riley <galenriley@gmail.com>
2020-01-23 18:08:19 -0500

    avoid server address config trailing slash issue

--- src/services/baseService.js
+++ src/services/baseService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { HOOLIGAN_HYMNAL_SERVER_ADDRESS } from '../config/Settings';
 
 let API = axios.create({
-  baseURL: HOOLIGAN_HYMNAL_SERVER_ADDRESS,
+  baseURL: HOOLIGAN_HYMNAL_SERVER_ADDRESS.replace(/\/$/, ""),
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json'
