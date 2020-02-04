08c71ad7da38bac77dee3a9cf9f09c9691309cde
Galen Riley <galenriley@gmail.com>
2020-02-03 22:05:57 -0500

    delete wrapper progress

---
+++ src/components/PostImageDeleteWrapper.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    Image,
+    TouchableOpacity,
+    View
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+
+export default class PostImageDeleteWrapper extends React.Component {
+    render() {
+        return (
+            <View>
+                <Image style={{ width: 100, height: 100, resizeMode: "cover" }}
+                    source={{ uri: this.props.uri }} />
+                <TouchableOpacity
+                    onPress={() => {
+                        if (this.props.onPressDelete)
+                            this.props.onPressDelete(this.props.uri)
+                    }}>
+                    <Ionicons
+                        name="md-close"
+                        size={50}
+                        style={{ color: DefaultColors.Primary, backgroundColor: 'transparent', margin: 5 }}
+                    />
+                </TouchableOpacity>
+            </View>
+        )
+    }
+}
