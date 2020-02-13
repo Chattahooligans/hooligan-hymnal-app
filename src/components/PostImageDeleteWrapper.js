41fa06978e75c273332e6ac57a1df7294aea239b
Galen Riley <galenriley@gmail.com>
2020-02-13 17:05:08 -0500

    add metadata btn to image wrapper in post creation

--- src/components/PostImageDeleteWrapper.js
+++ src/components/PostImageDeleteWrapper.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {
     Image,
+    Modal,
     StyleSheet,
     TouchableOpacity,
     View
@@ -9,6 +10,11 @@ import { Ionicons } from '@expo/vector-icons';
 import { DefaultColors } from '../config/Settings';
 
 export default class PostImageDeleteWrapper extends React.Component {
+
+    onPressMetadata = () => {
+        alert("launch metadata form")
+    }
+
     render() {
         return (
             <View style={{ marginHorizontal: 2 }}>
@@ -26,6 +32,15 @@ export default class PostImageDeleteWrapper extends React.Component {
                         style={{ color: DefaultColors.Primary, backgroundColor: 'transparent' }}
                     />
                 </TouchableOpacity>
+                <TouchableOpacity
+                    style={styles.metadata}
+                    onPress={this.onPressMetadata}>
+                    <Ionicons
+                        name="md-code"
+                        size={25}
+                        style={{ color: DefaultColors.Primary, backgroundColor: 'transparent' }}
+                    />
+                </TouchableOpacity>
             </View>
         )
     }
@@ -35,6 +50,19 @@ const styles = StyleSheet.create({
     delete: {
         position: "absolute",
         top: 2,
+        right: 2,
+        backgroundColor: DefaultColors.Secondary,
+        width: 25,
+        height: 25,
+        borderColor: DefaultColors.Primary,
+        borderWidth: 1,
+        borderRadius: 50,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    metadata: {
+        position: "absolute",
+        bottom: 2,
         right: 2,
         backgroundColor: DefaultColors.Secondary,
         width: 25,
