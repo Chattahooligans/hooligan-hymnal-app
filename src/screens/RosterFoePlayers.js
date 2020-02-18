301bee982891a9f60a56fcddb0b039527e77de28
Galen Riley <galenriley@gmail.com>
2020-02-18 17:38:14 -0500

    add accent color to Foes

--- src/screens/RosterFoePlayers.js
+++ src/screens/RosterFoePlayers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Animated, Image, FlatList, Platform, ScrollView, SectionList, StyleSheet, View} from 'react-native';
+import { Animated, Image, FlatList, Platform, ScrollView, SectionList, StyleSheet, View } from 'react-native';
 import FadeIn from 'react-native-fade-in-image';
 import Constants from 'expo-constants';
 import { Ionicons } from '@expo/vector-icons';
@@ -20,14 +20,14 @@ class FoePlayerRow extends React.Component {
         const { item: player } = this.props.item;
 
         return (
-            <View style={[styles.playerRow, {backgroundColor: this.props.backgroundColor}]}>
-              <View style={{ width: 25, alignItems: 'flex-end' }}>
-                <BoldText style={{ color: this.props.textColor }}>{player.squadNumber}</BoldText>
-              </View>
-              <View style={{ flexDirection: i18n.getFlexDirection() }}>
-                <BoldText style={{ color: this.props.textColor }}> | {player.name} </BoldText>
-                <RegularText style={{ color: this.props.textColor }}>({player.position})</RegularText>
-              </View>
+            <View style={[styles.playerRow, { backgroundColor: this.props.backgroundColor }]}>
+                <View style={{ width: 25, alignItems: 'flex-end' }}>
+                    <BoldText style={{ color: this.props.textColor }}>{player.squadNumber}</BoldText>
+                </View>
+                <View style={{ flexDirection: i18n.getFlexDirection() }}>
+                    <BoldText style={{ color: this.props.textColor }}> | {player.name} </BoldText>
+                    <RegularText style={{ color: this.props.textColor }}>({player.position})</RegularText>
+                </View>
             </View>
         )
     }
@@ -40,11 +40,11 @@ class RosterFoePlayers extends React.Component {
         backgroundColor: DefaultColors.ButtonBackground,
         accentColor: DefaultColors.ButtonText,
         textColor: DefaultColors.ButtonText
-      };
-    
+    };
+
     _renderItem = item => {
-        return ( <FoePlayerRow item={item} onPress={this._handlePressFoeButton} 
-                    backgroundColor={this.state.backgroundColor} accentColor={this.state.accentColor} textColor={this.state.textColor} /> )
+        return (<FoePlayerRow item={item} onPress={this._handlePressFoeButton}
+            backgroundColor={this.state.backgroundColor} accentColor={this.state.accentColor} textColor={this.state.textColor} />)
     }
 
     componentDidMount = () => {
@@ -54,96 +54,102 @@ class RosterFoePlayers extends React.Component {
         let textColor = DefaultColors.ButtonText;
 
         if (foe.backgroundColor)
-            backgroundColor = foe.backgroundColor;        
+            backgroundColor = foe.backgroundColor;
         if (foe.accentColor)
             accentColor = foe.accentColor;
         if (foe.textColor)
             textColor = foe.textColor;
 
-        this.setState({foe, backgroundColor, accentColor, textColor});
+        this.setState({ foe, backgroundColor, accentColor, textColor });
     }
 
     render() {
         const { scrollY } = this.state;
         const scale = scrollY.interpolate({
-        inputRange: [-300, 0, 1],
-        outputRange: [2, 1, 1],
-        extrapolate: 'clamp'
+            inputRange: [-300, 0, 1],
+            outputRange: [2, 1, 1],
+            extrapolate: 'clamp'
         });
         const translateX = 0;
         const translateY = scrollY.interpolate({
-        inputRange: [-300, 0, 1],
-        outputRange: [-50, 1, 1],
-        extrapolate: 'clamp'
+            inputRange: [-300, 0, 1],
+            outputRange: [-50, 1, 1],
+            extrapolate: 'clamp'
         });
 
         const headerOpacity = scrollY.interpolate({
-        inputRange: [0, 30, 200],
-        outputRange: [0, 0, 1]
+            inputRange: [0, 30, 200],
+            outputRange: [0, 0, 1]
         });
 
         let foe = this.state.foe;
 
-        let foeImage = 
+        let foeImage =
             <View>
                 <Ionicons name={Skin.Roster_FoesTabIcon} color={DefaultColors.ButtonBackground} size={150} />
-                <BoldText style={{color: DefaultColors.ColorText, marginBottom: 10}}>{foe.opponent}</BoldText>
+                <BoldText style={{ color: DefaultColors.ColorText, marginBottom: 10 }}>{foe.opponent}</BoldText>
             </View>
         if (foe.logo) {
-            let logoUri = {uri: foe.logo};
+            let logoUri = { uri: foe.logo };
             foeImage = <Animated.View
                 style={{
                     transform: [{ scale }, { translateX }, { translateY }]
                 }}
-                >
-                    <FadeIn>
-                        <Image
+            >
+                <FadeIn>
+                    <Image
                         source={logoUri}
                         style={styles.logo}
                         resizeMode='contain'
-                        />
-                    </FadeIn>
-                </Animated.View>
+                    />
+                </FadeIn>
+            </Animated.View>
         }
 
         return (
-          <LoadingPlaceholder>
-              <ScrollView>
-              <View style={styles.imageContainer}>{foeImage}</View>
-              <FlatList
-                    renderScrollComponent={props => <ScrollView {...props} />}
-                    data={foe.players}
-                    renderItem={this._renderItem}
-                    keyExtractor={(item, index) => index.toString()}
+            <LoadingPlaceholder>
+                <ScrollView>
+                    <View style={styles.imageContainer}>{foeImage}</View>
+                    {(foe.players && (foe.players.length > 0)) &&
+                        <View style={{ flex: 1, flexDirection: "row", height: 3, backgroundColor: foe.accentColor }} />
+                    }
+                    <FlatList
+                        renderScrollComponent={props => <ScrollView {...props} />}
+                        data={foe.players}
+                        renderItem={this._renderItem}
+                        keyExtractor={(item, index) => index.toString()}
                     />
+                    {(foe.players && (foe.players.length > 0)) &&
+                        <View style={{ flex: 1, flexDirection: "row", height: 3, backgroundColor: foe.accentColor }} />
+                    }
                 </ScrollView>
                 <NavigationBar
                     animatedBackgroundOpacity={headerOpacity}
                     paddingTop={0}
-                    style={[{paddingTop: 0},
-                        Platform.OS === 'android'
+                    style={[{ paddingTop: 0 },
+                    Platform.OS === 'android'
                         ? { height: Layout.headerHeight + Constants.statusBarHeight }
                         : null
                     ]}
                     renderLeftButton={() => (
                         <View
-                        style={{
-                            // gross dumb things
-                            paddingTop: Platform.OS === 'android' ? 10 : 0,
-                            marginTop: Layout.notchHeight > 0 ? -5 : 0
-                        }}
+                            style={{
+                                // gross dumb things
+                                paddingTop: Platform.OS === 'android' ? 10 : 0,
+                                marginTop: Layout.notchHeight > 0 ? -5 : 0
+                            }}
                         >
-                        <HeaderBackButton
-                            onPress={() => this.props.navigation.goBack()}
-                            tintColor={DefaultColors.ButtonBackground}
-                            title={null}
-                        />
+                            <HeaderBackButton
+                                onPress={() => this.props.navigation.goBack()}
+                                tintColor={DefaultColors.ButtonBackground}
+                                title={null}
+                            />
                         </View>
                     )}
-                    />
-          </LoadingPlaceholder>
+                />
+            </LoadingPlaceholder>
         );
-      }
+    }
 }
 
 const styles = StyleSheet.create({
