f610e12aa200e5804e00ae550de2640e01a7948c
Galen Riley <galenriley@gmail.com>
2020-02-25 15:19:36 -0500

    add ParsedTextHelper

---
+++ src/components/ParsedTextHelper.js
@@ -0,0 +1,24 @@
+import { Linking, StyleSheet } from 'react-native'
+
+export const parsePatterns = {
+    bold: /(\*)(.*?)\1/,
+    italic: /(_)(.*?)\1/,
+    hashtag: /\B#(\w*[A-Za-z_\d]+\w*)\b/
+}
+
+export function renderBoldItalic(match) { return match.slice(1, match.length - 1) }
+export function onUrlPress(url) { Linking.openURL(url) }
+export function onEmailPress(email) { Linking.openURL('mailto:' + email) }
+
+export const parsedStyles = StyleSheet.create({
+    bold: {
+        fontWeight: 'bold'
+    },
+    italic: {
+        fontStyle: 'italic'
+    },
+    url: {
+        color: Skin.Post_LinkColor,
+        textDecorationLine: 'underline'
+    }
+})
