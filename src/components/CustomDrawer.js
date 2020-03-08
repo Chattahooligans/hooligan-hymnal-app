03d160fa9843add7438b299e75925c4e9487a761
Galen Riley <galenriley@gmail.com>
2020-03-07 19:08:27 -0500

    add padding before cards in drawer

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -28,7 +28,7 @@ const CustomDrawer = props => (
         />
       </View>
     </View>
-    <ScrollView>
+    <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flex: 1, justifyContent: "space-between" }}>
       <DrawerItems
         {...props}
         activeBackgroundColor="rgba(255,255,255,0.1)"
