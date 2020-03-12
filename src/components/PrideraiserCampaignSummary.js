2e4b51fc9c863a94d4ae12282df13061557dbd29
Galen Riley <galenriley@gmail.com>
2020-03-11 22:12:13 -0400

    stub home screen PrideraiserCampaignSummary

---
+++ src/components/PrideraiserCampaignSummary.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {
+    Dimensions,
+    Image,
+    Linking,
+    StyleSheet,
+    TouchableHighlight,
+    View
+} from 'react-native';
+import { BigButton } from '../components/BigButton';
+import { BoldText, RegularText, RegularTextMonospace } from './StyledText';
+import PrideraiserRainbowBar from './PrideraiserRainbowBar';
+import PostImageWrapper from './PostImageWrapper';
+import { PRIDERAISER_LOGO } from '../config/Settings';
+import { getCampaign } from '../services/prideraiserService';
+import i18n from "../../i18n";
+
+export default class PrideraiserCampaignSummary extends React.Component {
+
+    // TODO: Move a bunch of shit to settings
+
+
+    state = {
+        loadedCampaign: false,
+        campaign: {},
+        error: false,
+        errorDetail: null,
+        source: ""
+    }
+
+    componentWillMount = async () => {
+        try {
+            const campaign = await getCampaign(PRIDERAISER_CAMPAIGN_ID)
+
+            // sanity check to confirm the campaign ID is right
+            // campaigns that don't exist look like { "detail": "Not found." }
+            if (campaign.hasOwnProperty("id")) {
+                // populate analytics source from Skin
+                let source = ""
+                if (Skin.PostAttachmentComposePrideraiserMatch_AnalyticsSourcePrefix)
+                    source += Skin.PostAttachmentComposePrideraiserMatch_AnalyticsSourcePrefix + "-"
+                source += moment(new Date()).format(Skin.PostAttachmentComposePrideraiserMatch_AnalyticsSourceDateFormat)
+                if (Skin.PostAttachmentComposePrideraiserMatch_AnalyticsSourceSuffix)
+                    source += "-" + Skin.PostAttachmentComposePrideraiserMatch_AnalyticsSourceSuffix
+
+                this.setState({ loadedCampaign: true, campaign, source })
+            }
+            else
+                this.setState({ loadedCampaign: false, error: true, errorDetail: "Not found." })
+        }
+        catch (error) {
+            this.setState({ loadedCampaign: false, error: true, errorDetail: error })
+        }
+    }
+
+    render() {
+        // move this to settings
+        const coverPhotoParams = "?w=768&h=200&wm=pr&wmp=br"
+
+        if (!this.state.loadedCampaign)
+            return <View />
+        else
+            return (
+                <TouchableHighlight
+                    style={styles.container}
+                    underlayColor={'#fff'}>
+                    <PrideraiserRainbowBar />
+                    <RegularText>text</RegularText>
+                    <PostImageWrapper
+                        containerWidth={Dimensions.get("window").width}
+                        source={{ uri: this.state.campaign.cover_photo.original + coverPhotoParams }} />
+                </TouchableHighlight>
+            )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+    }
+});
