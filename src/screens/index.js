bd0c2284a0f557382640f73cd0238598e6e99640
Galen Riley <galenriley@gmail.com>
2020-05-28 14:45:06 -0400

    remove single-purpose webview screens

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,9 +2,7 @@ import About from './About';
 import CapoHome from './CapoHome';
 import CapoLogin from './CapoLogin';
 import Channel from './Channel';
-import Events from './Events';
 import Home from './Home';
-import Instrumentation from './Instrumentation';
 import Player from './Player';
 import PostAttach from './PostAttach';
 import PostAttachmentComposeSong from './PostAttachmentComposeSong';
@@ -21,15 +19,12 @@ import Roster from './Roster';
 import RosterFoePlayers from './RosterFoePlayers';
 import RosterFoes from './RosterFoes';
 import RosterHome from './RosterHome';
-import Shop from './Shop';
 import SinglePost from './SinglePost';
 import SingleSong from './SingleSong';
 import Songbook from './Songbook';
 import Songs from './Songs';
-import Standings from './Standings';
 import TableOfContentsInline from './TableOfContentsInline';
 import TwitterList from './TwitterList';
-import Volunteer from './Volunteer';
 import WebView from './WebView';
 
 export default {
@@ -37,9 +32,7 @@ export default {
   CapoHome,
   CapoLogin,
   Channel,
-  Events,
   Home,
-  Instrumentation,
   Player,
   PostAttach,
   PostAttachmentComposeSong,
@@ -56,14 +49,11 @@ export default {
   RosterFoePlayers,
   RosterFoes,
   RosterHome,
-  Shop,
   SinglePost,
   SingleSong,
   Songbook,
   Songs,
-  Standings,
   TableOfContentsInline,
   TwitterList,
-  Volunteer,
   WebView
 };
