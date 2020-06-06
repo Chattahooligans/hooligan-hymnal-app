d6b5a94c763c46a058a86bd045d4f15517ef4705
Galen Riley <galenriley@gmail.com>
2020-06-06 13:20:30 -0400

    fix postattchmentcomposesong typo

--- src/screens/PostAttachmentComposeSong.js
+++ src/screens/PostAttachmentComposeSong.js
@@ -27,18 +27,18 @@ export default class PostAttachmentComposeSong extends React.Component {
         return (
             <View style={{ flex: 1 }}>
                 <KeyboardAvoidingView behavior="height" style={styles.container}>
-                    <BoldText style={{ textAlign: 'center' }}>{i18n.t('screens.postattchmentcomposesong.composesong')}</BoldText>
+                    <BoldText style={{ textAlign: 'center' }}>{i18n.t('screens.postattachmentcomposesong.composesong')}</BoldText>
                     <TextInput
                         style={styles.titleInput}
-                        placeholder={i18n.t('screens.postattchmentcomposesong.title')}
+                        placeholder={i18n.t('screens.postattachmentcomposesong.title')}
                         onChangeText={(text) => this.setState({ title: text })} />
                     <TextInput
                         style={styles.lyricsInput}
                         multiline={true}
-                        placeholder={i18n.t('screens.postattchmentcomposesong.lyrics')}
+                        placeholder={i18n.t('screens.postattachmentcomposesong.lyrics')}
                         onChangeText={(text) => this.setState({ lyrics: text })} />
                     <BigButton
-                        label={i18n.t('screens.postattchmentcomposesong.attach')}
+                        label={i18n.t('screens.postattachmentcomposesong.attach')}
                         iconName="md-add" iconPosition="right"
                         onPress={() => {
                             if (this.props.route.params.onAttachmentComplete)
