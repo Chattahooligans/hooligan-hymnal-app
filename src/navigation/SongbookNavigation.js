09faca5da9ef186fc0eea28d182bd6be6b2aab05
Galen Riley <galenriley@gmail.com>
2020-06-17 15:17:06 -0400

    added some comments on TabNavigator differences

--- src/navigation/SongbookNavigation.js
+++ src/navigation/SongbookNavigation.js
@@ -8,7 +8,22 @@ import Screens from '../screens';
 import i18n from '../i18n';
 
 const SongbookStackContainer = createStackNavigator();
-const SongbookTabs = createMaterialTopTabNavigator();
+//const SongbookTabs = createMaterialTopTabNavigator();
+const SongbookTabs = createBottomTabNavigator();
+/*
+    Notes on implementation:
+    MaterialTopTabNavigator lets us swipe from the ToC to the first song (but not back)
+        and it's a little slow on the transition from the cover to the ToC while all the tabs 
+        un-lazily render (which might be fixable?)
+    BottomTabNavigator, iOS style, is a bit faster on initial render, but there's a hitch
+        where half the ToC renders before the rest does.
+
+    Both have advantages and tradeoffs. The JSX element below has props for either implementation,
+        so just use the one that suits your mood, until a real winner emerges.
+
+    As of release 2.0.0 (the first with this Songbook UI), 
+        I chose MaterialTop to give us the swipe gesture to get out of ToC
+*/
 
 function SongbookTabsNavigation() {
     return (
