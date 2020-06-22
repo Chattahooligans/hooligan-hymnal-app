b9e5674adfea033241608f013f903a1a01a35926
Galen Riley <galenriley@gmail.com>
2020-06-22 07:50:11 -0400

    add platform version to about screen

--- src/screens/About.js
+++ src/screens/About.js
@@ -95,7 +95,7 @@ class About extends React.Component {
           <ParsedText
             parse={parsedTextOptions}
             style={[styles.credits, { textAlign: i18n.getRTLTextAlign(), writingDirection: i18n.getWritingDirection() }]}>
-            {i18n.t('screens.about.appTitle') + i18n.t('screens.about.hymnalplug')}
+            {i18n.t('screens.about.appTitle') + i18n.t('screens.about.hymnalplug1') + appParams.expo.extra.hooliganHymnalVersion + "\n\n" + i18n.t('screens.about.hymnalplug2')}
           </ParsedText>
           <View style={{ height: 20 }} />
           <ScrollView style={{ flex: 1 }}>
