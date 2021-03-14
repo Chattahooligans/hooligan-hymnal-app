d3ddb71d5805ef216e3bfad180df4490df32c1de
Galen Riley <galenriley@gmail.com>
2021-03-13 20:11:55 -0500

    cleanup ancient handler

--- src/containers/GlobalDataContainer.js
+++ src/containers/GlobalDataContainer.js
@@ -173,26 +173,6 @@ export default class GlobalDataContainer extends Container {
     this._setLocation(location);
   };
 
-  handleNotification = (notification) => {
-    if (notification.origin === "selected") {
-      // notification was tapped, either from the app already open or from entering the app
-      console.log("SELECTED notification", notification.data.song.title);
-
-      // TODO: open SingleSong screen and send it the Song object buried inside the notification
-
-      // kinda like this but it doesn't work from this far out (because navigation doesn't exist yet)
-      // this.props.navigation.navigate('SingleSong', {song: notification.data.song});
-
-      // Maybe set app state and do something with it that way?
-      // this.setState({ notification: notification });
-    } else if (notification.origin === "received") {
-      // notification was received, either app was already open or it just opened up but not from the notification
-      // no way to tell which?
-      // console.log('RECEIVED notification', notification.data.song.title);
-      // We don't necessarily want to do anything in this case
-    }
-  };
-
   registerForPushNotificationsAsync = async () => {
     try {
       const { status: existingStatus } = await Permissions.getAsync(
