d8a8305fd7bdf541a494f94b01ecfdc8d0a01ed8
Galen Riley <galenriley@gmail.com>
2021-04-15 21:58:53 -0400

    shuffle InstagramList_AppendHandles in alpha order

--- example-config.js
+++ example-config.js
@@ -512,15 +512,15 @@ export const Settings = {
   //      Hero content on the home screen
   Home_HeroContent: "video",
 
+  // Home_PostsPerPage: number
+  //      Load this many news feed items, then load this many more if the user scrolls to the bottom
+  Home_PostsPerPage: 5,
+
   // InstagramList_AppendHandles: string
   //      Extra Instagram handles concatenated after the players on the InstagramList screen
   //      Each account should be seperated by a space
   //      In Chattanooga, we append the club and our SG accounts. You can choose to pass.
   InstagramList_AppendHandles: "",
-
-  // Home_PostsPerPage: number
-  //      Load this many news feed items, then load this many more if the user scrolls to the bottom
-  Home_PostsPerPage: 5,
 
   // Player_ShowSongs: true, false
   //      Some SGs write songs for each player
